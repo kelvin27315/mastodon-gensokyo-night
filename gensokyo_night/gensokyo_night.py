6efc3d1e26d7dd766996f399e6ec4a6ad853d509
kelvin27315 <seven_units@yahoo.co.jp>
2022-02-12 21:58:45 +0900

    test書いた

--- gensokyo_night/gensokyo_night.py
+++ gensokyo_night/gensokyo_night.py
@@ -1,5 +1,6 @@
 import datetime
 from os import path
+from typing import List
 
 import pandas as pd
 from mastodon import Mastodon
@@ -12,23 +13,39 @@ if __name__ == "__main__":
         api_base_url="https://gensokyo.town",
     )
 
-# CSVからキャラクタ名ランダムに抽出し
-df = pd.read_csv(PATH + "../character.csv")
-sample_1 = df[df["label"] == 1].sample(3)
-sample_2 = df[df["label"] == 2].sample(1)
-sample = pd.concat([sample_1, sample_2])
-del sample["label"]
-np_sample = sample.values
 
-# 今日の日付けを取得し、日を1つ進める
-today = datetime.date.today() + datetime.timedelta(days=1)
+def read_csv(file_path: str) -> pd.DataFrame:
+    df = pd.read_csv(PATH + file_path)
+    return df
 
-# 投稿する文章を作成、投稿
-sent = "次回:" + str(today.month) + "月" + str(today.day) + "日のお題は\n"
-for i in np_sample:
-    sent += "・" + str(i) + "\n"
-sent = sent.replace("['", "")
-sent = sent.replace("']", "")
-sent += "です。各種投稿形式に応じて以下のタグを使い分けてくさだい。\n#幻想郷深夜のお絵描き一本勝負\n#幻想郷深夜の東方MMD一本勝負\n#幻想郷深夜の物書き一本勝負"
 
-mastodon.toot(sent)
+def sample_characters(df: pd.DataFrame) -> List[str]:
+    """pd.DataFrameからキャラクタ名ランダムに抽出"""
+    sample_1 = df[df["label"] == 1].sample(3)
+    sample_2 = df[df["label"] == 2].sample(1)
+    samples = pd.concat([sample_1, sample_2])
+    character_names: List[str] = samples["name"].tolist()
+    return character_names
+
+
+def post_toot(np_sample: List[str]) -> None:
+    """投稿する文章を作成、投稿"""
+    today = datetime.date.today() + datetime.timedelta(days=1)
+
+    text = "次回:" + str(today.month) + "月" + str(today.day) + "日のお題は\n"
+    for i in np_sample:
+        text += "・" + str(i) + "\n"
+    text += "です。各種投稿形式に応じて以下のタグを使い分けてくさだい。\n#幻想郷深夜のお絵描き一本勝負\n#幻想郷深夜の東方MMD一本勝負\n#幻想郷深夜の物書き一本勝負"
+
+    mastodon.toot(text)
+
+
+def main() -> None:
+    file_path = "../character.csv"
+    characters = read_csv(file_path)
+    character_names = sample_characters(characters)
+    post_toot(character_names)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_gensokyo_night.py
@@ -0,0 +1,53 @@
+import unittest
+from datetime import date, timedelta
+from unittest.mock import Mock
+
+import mastodon
+import pandas as pd
+
+from gensokyo_night import gensokyo_night as gn
+
+TOMORROW_STR = "{dt.month}月{dt.day}日".format(dt=date.today() + timedelta(days=1))
+
+
+class TestGensokyoNight(unittest.TestCase):
+    def setUp(self) -> None:
+        # gn.mastodon をmockするので、元のが定義されていたら退避
+        self.original_mastodon = gn.mastodon if hasattr(gn, "mastodon") else None
+        gn.mastodon = Mock(spec=mastodon.Mastodon)
+
+    def tearDown(self) -> None:
+        gn.mastodon = self.original_mastodon
+
+    def test_sample_characters(self) -> None:
+        characters = pd.DataFrame(
+            {
+                "name": [
+                    "博麗 靈夢 (旧作)",
+                    "霧雨 魔理沙 (旧作)",
+                    "十六夜 咲夜",
+                    "魂魄 妖夢",
+                    "東風谷 早苗",
+                    "稗田 阿求",
+                ],
+                "label": [2, 2, 1, 1, 1, 1],
+            }
+        )
+        character_names = gn.sample_characters(characters)
+        self.assertEqual(4, len(character_names))
+
+    def test_post_toot(self) -> None:
+        gn.post_toot(["霧雨 魔理沙", "十六夜 咲夜", "魂魄 妖夢", "博麗 靈夢 (旧作)"])
+        gn.mastodon.toot.assert_called_once_with(
+            "次回:"
+            + TOMORROW_STR
+            + "のお題は\n"
+            + "・霧雨 魔理沙\n"
+            + "・十六夜 咲夜\n"
+            + "・魂魄 妖夢\n"
+            + "・博麗 靈夢 (旧作)\n"
+            + "です。各種投稿形式に応じて以下のタグを使い分けてくさだい。\n"
+            + "#幻想郷深夜のお絵描き一本勝負\n"
+            + "#幻想郷深夜の東方MMD一本勝負\n"
+            + "#幻想郷深夜の物書き一本勝負"
+        )
